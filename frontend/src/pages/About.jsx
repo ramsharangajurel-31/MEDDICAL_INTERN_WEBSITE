47f67854314f62789844ee0dffd10d6645bb7f54
Ramsharan Gajurel <ramsharangajurel9742@gmail.com>
2025-09-13 15:09:29 +0545

    Updated

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import AboutBanner from '../components/AboutBanner.jsx';
 import Aboutpage from '../components/Aboutpage.jsx';
-import Testimonial from '../components/Testimonial.jsx';
+import Testimonial from '../components/testimonial.jsx';
 import ContactInfoCards from '../components/ContactInfoCards.jsx';
 import DoctorSlider from '../components/DoctorSlider.jsx';
 
